7736f93728af421e2fbe1f89d183793f9161b2bf
AndrewLicona <andrewlicona70@gmail.com>
2026-01-28 22:08:49 -0500

    fix(deploy): cambiar bcrypt por bcryptjs para eliminar dependencias nativas y arreglar build en Docker

--- app/backend/src/usuarios/usuarios.service.ts
+++ app/backend/src/usuarios/usuarios.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { UsuarioRepository } from './infrastructure/persistence/usuario.repository';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsuariosService {
